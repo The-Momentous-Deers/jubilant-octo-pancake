2cc08abac59e6a3a51a4d6ee28c0aae841d6e3f9
Matt1634 <zewarcobra@gmail.com>
2018-11-24 13:31:57 +0000

    It works bby ;) gonna go suck julians mum now lololololololoololololololololoolol

--- command-handling/railsbankInterface.py
+++ command-handling/railsbankInterface.py
@@ -13,9 +13,39 @@ api_key = 'iyg7oiwx5c6t462y80vytq0qtotredni#u9cgcudk4b95czmprv809jmb2ltkd971j0d3
 class RailsbankRequest:
 
     def __init__(self):
+        # This needs to fetch the relevant data from the database
+        # data includes, ledgerID, benifactoryID, enduserID?
         response = get('v1/customer/me')
-        pprint.pprint(response)
+        # pprint.pprint(response)
         self.customer_id = response['customer_id']
+
+    def makeBeneficiary(self):
+        '''
+        Creating beneficiary for our enduser.
+        '''
+        response = post(
+            'v1/customer/beneficiaries', {
+                'holder_id': self.customer_id,
+                'asset_class': 'currency',
+                'asset_type': 'eur',
+                'iban': 'SK4402005678901234567890',
+                'bic_swift': 'SUBASKBX',
+                #'uk_account_number': '12345678',
+                #'uk_sort_code': '123434',
+                'person': {
+                    'name': 'Peter',
+                    'address': {
+                        'address_iso_country': 'SK'
+                    },
+                    'email': 'peter@email.com'
+                }
+            })
+        pprint.pprint(response)
+        self.beneficiary_id = response['beneficiary_id']
+        '''
+        Beneficiary is not ready immediately because of ongoing validity checks.
+        '''
+        response = get('v1/customer/beneficiaries/' + str(self.beneficiary_id) + '/wait')
 
     def getEnduser(self):
         response = post(
@@ -28,14 +58,14 @@ class RailsbankRequest:
                     }
                 }
             })
-        pprint.pprint(response)
+        #pprint.pprint(response)
         self.enduser_id = response['enduser_id']
 
         '''
         Enduser is not ready immediately because of ongoing validity checks.
         '''
         response = get('v1/customer/endusers/' + str(self.enduser_id) + '/wait')
-        pprint.pprint(response)
+        #pprint.pprint(response)
 
     def makeLedger(self):
         '''
@@ -44,7 +74,7 @@ class RailsbankRequest:
         bank_example_product = 'ExampleBank-EUR-1'
         response = post(
             'v1/customer/ledgers', {
-                'holder_id': self.customer_id, # this was self.enduser_id
+                'holder_id': self.enduser_id, # this was self.enduser_id
                 'partner_product': bank_example_product,
                 'asset_class': 'currency',
                 'asset_type': 'eur',
@@ -53,14 +83,90 @@ class RailsbankRequest:
                 'ledger-primary-use-types': ['ledger-primary-use-types-payments'],
                 'ledger-t-and-cs-country-of-jurisdiction': 'GB'
             })
-        pprint.pprint(response)
+        # pprint.pprint(response)
         self.ledger_id = response['ledger_id']
+        # pprint.pprint(response)
+
         pprint.pprint(response)
+        ledger_id = response['ledger_id']
+        time.sleep(10)
+        response = get('v1/customer/ledgers/' + str(ledger_id))
+        pprint.pprint(response)
+        response = post('v1/customer/ledgers/' + str(ledger_id) + '/assign-iban')
+        pprint.pprint(response)
+        response = get('v1/customer/ledgers/' + str(ledger_id) + '/wait')
+        pprint.pprint(response)
+        bic = response['bic_swift']
+        iban = response['iban']
+        '''
+        Crediting the ledger with 10 euro.
+        '''
+        response = post('dev/customer/transactions/receive', {
+            'iban': iban,
+            'bic-swift': bic,
+            'amount': 10
+        })
+        transaction_receive_id = response['transaction_id']
+        '''
+        We need to wait for the transaction to be approved.
+        '''
+        time.sleep(10)
+        response = get('v1/customer/transactions/' + str(transaction_receive_id))
+        pprint.pprint(response)
+
 
     def getBalance(self):
         response = get('v1/customer/ledgers/' + str(self.ledger_id))
+        pprint.pprint(response["amount"])
+
+    def makePayment(self):
+        '''
+        Creating 2 euro transaction from the ledger to the beneficiary.
+        '''
+        response = post(
+            'v1/customer/transactions', {
+                'ledger_from_id': self.ledger_id,
+                'beneficiary_id': self.beneficiary_id,
+                'payment_type': 'payment-type-EU-SEPA-Step2',
+                'amount': '2'
+                })
+        pprint.pprint(response)
+        transaction_id = response['transaction_id']
+
+        '''
+        We need to wait for the transaction to be approved.
+        '''
+        time.sleep(10)
+        response = get('v1/customer/transactions/' + str(transaction_id))
+        pprint.pprint(response)
+
+    def addMoney(self):
+        response = post(
+            'dev/customer/transactions/receive', {
+                'amount': 10,
+                'uk_account_number': '12345678',
+                'uk_sort_code': '123434'
+            })
+        '''
+        We need to wait for the transaction to be approved.
+        '''
+        transaction_receive_id = response['transaction_id']
+        time.sleep(10)
+        response = get('v1/customer/transactions/' + str(transaction_receive_id))
         pprint.pprint(response)
 
 myrequest = RailsbankRequest()
+
+# These are for getting the balance.
+myrequest.getEnduser()
 myrequest.makeLedger()
-myrequest.getBalance()
+#myrequest.addMoney() #not needed
+#myrequest.getBalance()
+print("WE ARE NOW DOING PAYMENT")
+# These are for making a payment
+myrequest.makeBeneficiary()
+myrequest.makePayment()
+# make payment
+# add benificary
+# request card
+# notify when money enters account
